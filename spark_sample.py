292786145e032b131dafb41a8e5019e04c038d5a
manabian- <lakers_voyage@yahoo.co.jp>
2022-03-24 12:45:38 +0900

    add

---
+++ spark_sample.py
@@ -0,0 +1,16 @@
+# Executing PySpark in VS Code using Databricks Connect
+import numpy as np
+import pandas as pd
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+nCols = 15
+nRows = 5
+columns = ["col"+str(i) for i in range(nCols)]
+ 
+# Create a pandas DF
+pandas_df = pd.DataFrame(np.random.randint(0,100, size=(nRows,nCols)))
+ 
+# Convert to Spark DF
+df = spark.createDataFrame(pandas_df).toDF(*columns)
+
+df.show()
---
+++ test.py
@@ -0,0 +1,20 @@
+from tashizan import tashizan
+import unittest
+
+
+class TestTashizan(unittest.TestCase):
+    """test class of tashizan.py
+    """
+
+    def test_tashizan(self):
+        """test method for tashizan
+        """
+        value1 = 2
+        value2 = 6
+        expected = 8
+        actual = tashizan(value1, value2)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
